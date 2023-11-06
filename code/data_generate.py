019b7e711a566abb56bf8074323ac7d8c13b9000
abdallahalseddek <abdallahalseddek@gmail.com>
2023-11-06 11:39:11 +0200

    update py generate script

--- code/data_generate.py
+++ code/data_generate.py
@@ -1,52 +1,15 @@
 import time
-import mysql.connector
-
-# Establish a connection to the database
-conn = mysql.connector.connect(user='your_user', password='your_password', database='your_database')
-cursor = conn.cursor()
-
-# Define the INSERT query template
-insert_query = "INSERT INTO userinfo (`name`, `email`, `password`, `dob`, `address`, `city`, `state_id`, `zip`, `country_id`, `account_type`, `closest_airport`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-# Start measuring time
-start_time = time.time()
-
-# Assuming you want to insert 1,000,000 rows
-max_rows = 1000000
+sql = "INSERT INTO userinfo (`name`, `email`, `password`, `dob`, `address`, `city`, `state_id`, `zip`, `country_id`, `account_type`, `closest_airport`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+params = [
+    "John", "john.smith@email.com", "1234", "1986-02-14",
+    "1795 Santiago de Compostela Way", "Austin", "1", "18743", "1", "customer account", "aut"
+]
 state_id = 0
-
-# Use a transaction for bulk insert
-try:
-    conn.start_transaction()
-
-    for x in range(max_rows):
+start_time = time.time()
+for x in range(0, 10000000):
+    if (x % 100 == 0):
         state_id += 1
-        values = (
-            f'John{x}',
-            'john.smith@email.com',
-            '1234',
-            '1986-02-14',
-            '1795 Santiago de Compostela Way',
-            'Austin',
-            state_id,
-            '18743',  # Split the zip and country_id
-            1,  # Provide a separate value for country_id
-            'customer account',
-            'aut'
-        )
-        cursor.execute(insert_query, values)
+    result = session.run_sql(sql, (params[0], params[1]+str(x) , params[2], params[3], params[4], params[5], state_id, params[7], params[8], params[9], params[10]))
 
-    conn.commit()  # Commit the transaction
-
-except Exception as e:
-    conn.rollback()  # Rollback the transaction if an error occurs
-    print(f"Error: {e}")
-
-finally:
-    cursor.close()
-    conn.close()
-
-# Calculate total time
 total_time = time.time() - start_time
 print("Total time: %s seconds" % total_time)
-
